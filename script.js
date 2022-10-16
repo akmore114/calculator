7d20ff92e1d12979c55af4fe5858912233072a14
akmore114 <51195613+akmore114@users.noreply.github.com>
2022-10-16 13:21:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+add(10,15);
+
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
